fabd99e73bca5534dfe980237b0f5f185994d3cc
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-01-18 16:50:09 -0300

    add: styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: #0d6efd;
+  margin-top: 40px;
+`;
+
 function App() {
   return (
     <div>
-      <h1>Mi primera web</h1>
-      <p>Hecha con React + TypeScript</p>
+      <Title>Mi primera web</Title>
+      <p className='lead'>Hecha con React + TypeScript</p>
     </div>
   )
 }
